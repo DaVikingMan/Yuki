a40d5811abc54c1045bd297d270c7c890cdf9818
DaVikingMan <shashwatsinha080@gmail.com>
2021-10-03 15:21:20 +0530

    Change text in std::cout Signed-off by : DaVikingMan

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 
+class Type
+{
+	public:
+		int x;
+		bool truerun;
+		bool successrun;
+		float successrate;
+
+};
+
 int main()
 {
 
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Starting Yuki :: FalconXOS :: UI :: ScreenUI" << std::endl;
+	std::cout << "Nothing to execute, process tree clean" << std::endl;
+	
 }
+
 
